a69853edf70b4a12982a17240dfe42f424662f20
mws262 <mws262@cornell.edu>
2018-09-05 09:36:59 -0400

    adding color to utests

--- unit_test/test_general_interpolator.m
+++ unit_test/test_general_interpolator.m
@@ -3,63 +3,71 @@ function test_general_interpolator()
 tolerance = 1e-15;
 
 theader('Testing general_interpolator');
-
-%% Single dimension
-tname('Single dimensional test');
-single_dim_interp = general_interpolator(1);
-tspan1 = linspace(-1, 5, 12)';
-dspan1 = rand(size(tspan1));
-single_dim_interp.add_to_end_at_time(tspan1, dspan1);
-assert_near(single_dim_interp.get_at_time(3), interp1(tspan1, dspan1, 3), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
-assert_near(single_dim_interp.get_at_time(-0.99), interp1(tspan1, dspan1, -0.99), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
-assert_near(single_dim_interp.get_at_time(-1), interp1(tspan1, dspan1, -1), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
-assert_near(single_dim_interp.get_at_time(5), interp1(tspan1, dspan1, 5), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
-
-% Add another element.
-added_ele_time = 6;
-added_ele_val = 4;
-single_dim_interp.add_to_end_at_time(added_ele_time, added_ele_val);
-assert_near(single_dim_interp.get_at_time(5.5), (dspan1(end) + added_ele_val)/2, tolerance, 'adding a single element to the end of an existing dataset produced bad interpoation values in between old and new in 1d case.');
-
-% Adding overlapping data should throw.
-assert_error(@()(single_dim_interp.add_to_end_at_time(0, 1)));
-assert_error(@()(single_dim_interp.add_to_end_at_time(-2, 1)));
-assert_error(@()(single_dim_interp.add_to_end_at_time(-1, 1)));
-assert_error(@()(single_dim_interp.add_to_end_at_time(6, 1)));
-
-%% 2D
-tname('2D test');
-dim = 4;
-two_d_interp = general_interpolator(dim);
-tspan2 = linspace(-1, 5, 12)';
-dspan2 = rand(size(tspan2,1),dim);
-two_d_interp.add_to_end_at_time(tspan2,dspan2);
-
-assert_near(two_d_interp.get_at_time(3), interp1(tspan2, dspan2, 3), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
-assert_near(two_d_interp.get_at_time(-0.99), interp1(tspan2, dspan2, -0.99), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
-assert_near(two_d_interp.get_at_time(-1), interp1(tspan2, dspan2, -1), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
-assert_near(two_d_interp.get_at_time(5), interp1(tspan2, dspan2, 5), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
-
-% Add another element.
-added_ele_time = 6;
-added_ele_val = rand(1,dim);
-two_d_interp.add_to_end_at_time(added_ele_time, added_ele_val);
-assert_near(two_d_interp.get_at_time(5.5), (dspan2(end,:) + added_ele_val)/2, tolerance, 'adding a single element to the end of an existing dataset produced bad interpoation values in between old and new in 2d case.');
+do_test(@single_dim);
+e = do_test(@two_d);
+do_test(@three_d);
 
 
-%% 3D
-tname('3D case');
-dim = [3,3];
-three_d_interp = general_interpolator(dim);
-tspan3 = linspace(-1,5,12)';
-dspan3 = rand([3, 3, length(tspan3)]);
+    function single_dim()
+        %% Single dimension
+        tname('Single dimensional test');
+        single_dim_interp = general_interpolator(1);
+        tspan1 = linspace(-1, 5, 12)';
+        dspan1 = rand(size(tspan1));
+        single_dim_interp.add_to_end_at_time(tspan1, dspan1);
+        assert_near(single_dim_interp.get_at_time(3), interp1(tspan1, dspan1, 3), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
+        assert_near(single_dim_interp.get_at_time(-0.99), interp1(tspan1, dspan1, -0.99), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
+        assert_near(single_dim_interp.get_at_time(-1), interp1(tspan1, dspan1, -1), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
+        assert_near(single_dim_interp.get_at_time(5), interp1(tspan1, dspan1, 5), tolerance, 'general_interpolator does not agree with interp1 in 1d case.');
+        
+        % Add another element.
+        added_ele_time = 6;
+        added_ele_val = 4;
+        single_dim_interp.add_to_end_at_time(added_ele_time, added_ele_val);
+        assert_near(single_dim_interp.get_at_time(5.5), (dspan1(end) + added_ele_val)/2, tolerance, 'adding a single element to the end of an existing dataset produced bad interpoation values in between old and new in 1d case.');
+        
+        % Adding overlapping data should throw.
+        assert_error(@()(single_dim_interp.add_to_end_at_time(0, 1)));
+        assert_error(@()(single_dim_interp.add_to_end_at_time(-2, 1)));
+        assert_error(@()(single_dim_interp.add_to_end_at_time(-1, 1)));
+        assert_error(@()(single_dim_interp.add_to_end_at_time(6, 1)));
+    end
 
-three_d_interp.add_to_end_at_time(tspan3,dspan3);
-% assert_near(three_d_interp.get_at_time(3), interp1(tspan2, dspan2, 3), tolerance, 'general_interpolator does not agree with interp1 in 3d case.');
+    function two_d()
+        %% 2D
+        tname('2D test');
+        dim = 4;
+        two_d_interp = general_interpolator(dim);
+        tspan2 = linspace(-1, 5, 12)';
+        dspan2 = rand(size(tspan2,1),dim);
+        two_d_interp.add_to_end_at_time(tspan2,dspan2);
+        
+        assert_near(two_d_interp.get_at_time(3), interp1(tspan2, dspan2, 2), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
+        assert_near(two_d_interp.get_at_time(-0.99), interp1(tspan2, dspan2, -0.99), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
+        assert_near(two_d_interp.get_at_time(-1), interp1(tspan2, dspan2, -1), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
+        assert_near(two_d_interp.get_at_time(5), interp1(tspan2, dspan2, 5), tolerance, 'general_interpolator does not agree with interp1 in 2d case.');
+        
+        % Add another element.
+        added_ele_time = 6;
+        added_ele_val = rand(1,dim);
+        two_d_interp.add_to_end_at_time(added_ele_time, added_ele_val);
+        assert_near(two_d_interp.get_at_time(5.5), (dspan2(end,:) + added_ele_val)/2, tolerance, 'adding a single element to the end of an existing dataset produced bad interpoation values in between old and new in 2d case.');
+    end
 
-% TODO TOMORROW.
-
-
+    function three_d()
+        %% 3D
+        tname('3D case');
+        dim = [3,3];
+        three_d_interp = general_interpolator(dim);
+        tspan3 = linspace(-1,5,12)';
+        dspan3 = rand([3, 3, length(tspan3)]);
+        
+        three_d_interp.add_to_end_at_time(tspan3,dspan3);
+        % assert_near(three_d_interp.get_at_time(3), interp1(tspan2, dspan2, 3), tolerance, 'general_interpolator does not agree with interp1 in 3d case.');
+        
+        % TODO TOMORROW.
+        
+    end
 
 end
 
--- unit_test/tname.m
+++ unit_test/tname.m
@@ -1,3 +1,3 @@
 function tname(message)
-fprintf('Testing: %s. \n', message);
+fprintf('Testing: %-25.25s... \t\t', message);
 end
